6b6ac321edcfe0ff7130051c3d27070228aee69a
root <2365252385@qq.comclear>
2023-05-20 10:13:15 +0800

    train4 2

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import neptune
 
 use_gpu = True
 lr = 0.001
-batch_size = 64
-epochs = 1
+batch_size = 32
+epochs = 20
 
 
 def init():
     global run
-    run = neptune.init_run(project="conql/Amazons", api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZDMxZjA4Ny1lOTYzLTQ2YjUtYTg1My1iMjQxNDQ3ZTZkMDEifQ==")
+    run = neptune.init_run(project="conql/Amazons", api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZDMxZjA4Ny1lOTYzLTQ2YjUtYTg1My1iMjQxNDQ3ZTZkMDEifQ==", capture_stdout=True, capture_stderr=True, capture_hardware_metrics=True)
     run["use_gpu"] = use_gpu
     run["lr"] = lr
     run["batch_size"] = batch_size
