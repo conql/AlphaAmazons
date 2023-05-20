12871d64ff46d560469df31b6be96edb0c296246
conql <huanggqiang@live.cn>
2023-05-20 10:28:39 +0800

    neptune config

--- config.py
+++ config.py
@@ -11,7 +11,14 @@ model_name = "resnet_02"
 
 def init():
     global run
-    run = neptune.init_run(project="conql/Amazons", api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZDMxZjA4Ny1lOTYzLTQ2YjUtYTg1My1iMjQxNDQ3ZTZkMDEifQ==")
+    run = neptune.init_run(
+        project="conql/Amazons",
+        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZDMxZjA4Ny1lOTYzLTQ2YjUtYTg1My1iMjQxNDQ3ZTZkMDEifQ==",
+        capture_stdout=True,
+        capture_stderr=True,
+        capture_hardware_metrics=True,
+        capture_hardware_gpu_metrics=True,
+    )
     run["use_gpu"] = use_gpu
     run["lr"] = lr
     run["batch_size"] = batch_size
@@ -19,4 +26,3 @@ def init():
     run["residual_blocks"] = residual_blocks
     run["residual_channels"] = residual_channels
     run["model_name"] = model_name
-    
